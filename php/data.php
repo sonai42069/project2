b147e6fa59bcc160f59e5bd52d22a17603b4d018
Soumitra Halder <haldersoumitra23102@gmail.com>
2023-02-16 23:34:44 +0530

    change

--- php/data.php
+++ php/data.php
@@ -1,35 +1,15 @@
-<!-- <?php
+<?php
     while($row = mysqli_fetch_assoc($query)){
-        $sql2 = "SELECT * FROM messages WHERE (incoming_msg_id = {$row['unique_id']}
-                or outgoing_msg_id = {$row['unique_id']}) and (outgoing_msg_id = {$outgoing_id} 
-                or incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
-        $query2 = mysqli_query($conn,$sql2);
-        $use = mysqli_num_rows($query2);
-        if($use > 0){
-            $row2 = mysqli_fetch_assoc($query2);
-            if($row2['msg_id'] == $row['msg_id']){
-                $msg_id = $row['msg_id'];
-            }else{
-                $msg_id = $row2['msg_id'];
-            }
-            if($msg_id == $outgoing_id){
-                $outgoing_id = $row['outgoing_msg_id'];
-            }
-            if($msg_id == $incoming_id){
-                $incoming_id = $row['incoming_msg_id'];
-            }
-            if($msg_id == $outgoing_id || $msg_id == $incoming_
-                    || $msg_id == $outgoing_id){
-                        echo $msg_id;
-            }
-            
-
- //       $row2 = mysqli_fetch_assoc($query2);
- /*       (mysqli_num_rows($query2) > 0) ? $result = $row2['msg'] : $result ="No message available";
+        $sql2 = "SELECT * FROM massages WHERE (incoming_msg_id = {$row['unique_id']}
+                OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id} 
+                OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
+        $query2 = mysqli_query($conn, $sql2);
+        $row2 = mysqli_fetch_assoc($query2);
+        (mysqli_num_rows($query2) > 0) ? $result = $row2['msg'] : $result ="No message available";
         (strlen($result) > 28) ? $msg =  substr($result, 0, 28) . '...' : $msg = $result;
         if(isset($row2['outgoing_msg_id'])){
             ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
-        }*/else{
+        }else{
             $you = "";
         }
         ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
@@ -46,5 +26,6 @@
                     <div class="status-dot '. $offline .'"><i class="fas fa-circle"></i></div>
                 </a>';
     }
-}
-?>  
+ 
+
+?>
--- php/insert-chat.php
+++ php/insert-chat.php
@@ -4,9 +4,9 @@
         include_once "config.php";
         $outgoing_id = $_SESSION['unique_id'];
         $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
-        $message = mysqli_real_escape_string($conn, $_POST['message']);
+        $message = mysqli_real_escape_string($conn, $_POST['massage']);
         if(!empty($message)){
-            $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg)
+            $sql = mysqli_query($conn, "INSERT INTO massages (incoming_msg_id, outgoing_msg_id, msg)
                                         VALUES ({$incoming_id}, {$outgoing_id}, '{$message}')") or die();
         }
     }else{
